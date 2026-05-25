918ede217b6bc1e8debf5d75d5c9d627db32d33a
Rodolpho Loreto <rodolpho.loreto@hotmail.com>
2026-05-25 20:49:01 -0300

    Ajuste lógico na consulta de saldo

--- src/Modules/Consolidados/FinControl.Consolidado.Core/Features/Queries/GetSaldoConsolidado/GetSaldoConsolidadoQueryHandler.cs
+++ src/Modules/Consolidados/FinControl.Consolidado.Core/Features/Queries/GetSaldoConsolidado/GetSaldoConsolidadoQueryHandler.cs
@@ -18,7 +18,7 @@ namespace FinControl.Consolidado.Core.Features.Queries.GetSaldoConsolidado;
     public async Task<GetSaldoConsolidadoResponse> Handle(GetSaldoConsolidadoQuery request, CancellationToken cancellationToken)
     {
 
-
+        var dataAtual = DateOnly.FromDateTime(DateTimeOffset.UtcNow.UtcDateTime);
         var data = DateOnly.FromDateTime(request.DataLancamento?.ToDateTime(new TimeOnly(0, 0)) ?? DateTimeOffset.UtcNow.UtcDateTime);
         var key = CacheKey(data);
 
@@ -27,12 +27,38 @@ namespace FinControl.Consolidado.Core.Features.Queries.GetSaldoConsolidado;
         var saldoConsolidado = await cache.GetAsync<SaldoConsolidado>(key, cancellationToken);
 
         if (saldoConsolidado is null)
+        {
+
+            for (int i = 0; i < 30; i++)
+            {
+                data = data.AddDays(-1);
+                key = CacheKey(data);
+
+                if (logger.IsEnabled(LogLevel.Warning))
+                    logger.LogWarning(
+                        "Saldo consolidado não encontrado no cache. buscando o saldo anterior para inicialização do dia | Data={Data} Tentativa={Tentativa} CacheKey={CacheKey}",
+                        data,
+                        i + 1,
+                        key);
+
+                await Task.Delay(100, cancellationToken);
+                saldoConsolidado = await cache.GetAsync<SaldoConsolidado>(key, cancellationToken);
+
+                if (saldoConsolidado is not null)
+                {
+                    await cache.SetAsync(CacheKey(DateOnly.FromDateTime(request.DataLancamento?.ToDateTime(new TimeOnly(0, 0)) ?? DateTimeOffset.UtcNow.UtcDateTime)), saldoConsolidado, TimeSpan.FromDays(30), cancellationToken);
+                    break;
+                }
+            }
+        }
+        else
             saldoConsolidado = new SaldoConsolidado(0, DateTimeOffset.UtcNow);
+        
 
 
         GetSaldoConsolidadoResponse result = new(
-            Saldo: saldoConsolidado.Saldo,
-            UltimaAtualizacao: saldoConsolidado.UltimaAtualizacao
+            Saldo: saldoConsolidado?.Saldo ?? 0,
+            UltimaAtualizacao: saldoConsolidado?.UltimaAtualizacao ?? DateTimeOffset.UtcNow.UtcDateTime
         );
 
         if (logger.IsEnabled(LogLevel.Information))
